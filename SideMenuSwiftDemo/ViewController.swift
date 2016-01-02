5bd1b16058ec8328e4fdd7677a2696d29e7e8bea
kiran5232 <kiran.spaceo@gmail.com>
2016-01-02 12:18:56 +0530

    Initial Commit

---
+++ SideMenuSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideMenuSwiftDemo
+//
+//  Created by Kiran Patel on 1/2/16.
+//  Copyright © 2016  SOTSYS175. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
